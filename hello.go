d9d29810ea4f47dc8c895d49c74976a9d4fb83dc
rui-watanabe <wataru610806@gmail.com>
2021-03-07 12:42:23 +0900

    practice library

--- hello.go
+++ hello.go
@@ -1,13 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os/user"
+	"time"
+)
 
 func init() {
 	fmt.Println("init")
 }
 
 func hoge() {
-	fmt.Println("hoge")
+	fmt.Println("hoge", time.Now())
+	fmt.Println(user.Current)
 }
 
 func main() {
