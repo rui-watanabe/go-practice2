a3c09da291b2269130feeeb0e01493328ba2e17e
rui-watanabe <wataru610806@gmail.com>
2021-03-07 12:58:34 +0900

    practice variable

---
+++ variable.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+//関数内で宣言が可能
+var (
+	i    int     = 1
+	f64  float64 = 1.2
+	s    string  = "hoge"
+	t, f bool    = true, false
+)
+
+func localVariable() {
+	//関数内のみ宣言が可能
+	xi := 2
+	xi = 100               //再代入
+	var xf32 float32 = 1.3 //宣言をしたい場合はvarを使う
+	xs := "hogehoge"
+	xt, xf := true, false
+	fmt.Println(xi, xf32, xs, xt, xf)
+	fmt.Printf("%T\n", xf32)
+}
+
+func main() {
+	localVariable()
+	fmt.Println(i, f64, s, t, f)
+}
